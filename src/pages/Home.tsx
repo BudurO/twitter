55d491cb790be993b591982cf4928ff47e657767
amzan1 <amzanabdullah97@gmail.com>
2023-09-01 13:11:26 +0300

    done the side

---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Side from '../components/Side'
+
+function Home() {
+  return (
+    <div>
+        
+
+        <div className='flex justify-end'>
+        <Side/>
+
+        </div>
+    </div>
+  )
+}
+
+export default Home
