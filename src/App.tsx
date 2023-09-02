cc301b6f44a496288c5dda51351f39418a76e339
BudurO <buduralthubyani@gmail.com>
2023-09-02 20:31:15 +0300

    add path for Home and profile in navbar components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 
 import { Route, Routes } from 'react-router-dom'
-import './App.css'
 import Content from './components/Content'
-
 import Profile from './pages/Profile'
-
-
 import './App.css'
 import Home from './pages/Home'
 function App() {
@@ -13,6 +9,8 @@ function App() {
   return (
     <>
     <Routes>
+      <Route path='/' element={<Home/>}/>
+      
       <Route path='/profile' element={<Profile/>}/>
       <Route path='/profile/:id' element={<Content/>}/>
     </Routes>
